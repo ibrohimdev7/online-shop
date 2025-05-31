a6a51893194d99a224d38906ac5eb0b7b966c068
Ibrohim Qahramonov <qahramonovibrohimdev@gmail.com>
2025-05-31 16:28:48 +0500

    Add GitHub Actions for Railway CI/CD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const Home = async () => {
 
       <section className="flex flex-col space-y-12">
         <h1 className="text-xl sm:text-5xl font-bold text-center uppercase">
-          Halal shop deals
+          Halal shop deals (Test)
         </h1>
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products?.map((product) => (
